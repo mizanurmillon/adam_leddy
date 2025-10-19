912c5bfa826be5650d2ab472aefd989414132763
Rhishi-kesh <reshikash300@gmail.com>
2025-10-19 09:40:37 +0600

    owner revenue 60% and instructor revinue 40% added on dashboard calclutions

--- app/Http/Controllers/Web/Backend/DashboardController.php
+++ app/Http/Controllers/Web/Backend/DashboardController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Web\Backend;
 
 use Carbon\Carbon;
@@ -13,13 +14,14 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $monthlyUsersCount = User::where('role', 'student')->whereMonth('created_at', Carbon::now()->month)
-            ->whereYear('created_at', Carbon::now()->year)
+        $now = Carbon::now();
+        $monthlyUsersCount = User::where('role', 'student')->whereMonth('created_at', $now->month)
+            ->whereYear('created_at', $now->year)
             ->count();
         $totalUsers = User::where('role', 'student')->count();
 
-        $monthlyInstructorCount = User::where('role', 'instructor')->whereMonth('created_at', Carbon::now()->month)
-            ->whereYear('created_at', Carbon::now()->year)
+        $monthlyInstructorCount = User::where('role', 'instructor')->whereMonth('created_at', $now->month)
+            ->whereYear('created_at', $now->year)
             ->count();
         $totalInstructor = User::where('role', 'instructor')->count();
 
@@ -27,11 +29,11 @@ class DashboardController extends Controller
         $total_category = Category::count();
         $total_tag = Tag::count();
 
-        $total_revenue = MembershipHistory::sum('price');
+        $total_revenue = MembershipHistory::sum('price') * 0.6;
 
-        $monthlyRevenue = MembershipHistory::whereMonth('created_at', Carbon::now()->month)
-            ->whereYear('created_at', Carbon::now()->year)
-            ->sum('price');
+        $monthlyRevenue = MembershipHistory::whereMonth('created_at', $now->month)
+            ->whereYear('created_at', $now->year)
+            ->sum('price') * 0.6;
 
         return view('backend.layouts.index', compact('monthlyUsersCount', 'totalUsers', 'monthlyInstructorCount', 'totalInstructor', 'total_course', 'total_category', 'total_tag', 'total_revenue', 'monthlyRevenue'));
     }
